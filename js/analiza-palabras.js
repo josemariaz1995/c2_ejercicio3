af777d59040d6f88f7a55b8bb1bb1f9f64bf4fdc
Jose Maria Zamora Perez <jzamora@biada.net>
2021-05-27 18:02:58 +0200

    primer commit

--- js/analiza-palabras.js
+++ js/analiza-palabras.js
@@ -1,1 +1,43 @@
-function analizaPalabras(array) {}
+const array = ["hola", "Fotosíntesis", "Fusible", "yo"];
+const array2 = ["legible", "Inflamable", "comestible", "Array2"];
+function analizaPalabras(palabras) {
+  palabras.forEach((palabra, index) => {
+    console.log("#######");
+    console.log(`Palabra ${index + 1}: ${palabra}`);
+    console.log(`Nº de caracteres: ${palabra.length}`);
+    let snake = `${palabra.charAt(0)}`;
+    for (const i of palabra.slice(1)) {
+      snake += `-${i}`;
+    }
+    console.log(`Deletreo: ${snake}`);
+    let par = false;
+    let mayus = false;
+    if (palabra.length % 2 === 0) {
+      par = true;
+    } else {
+      par = false;
+    }
+    if (palabra.charAt(0) === palabra.charAt(0).toUpperCase()) {
+      mayus = true;
+    } else {
+      mayus = false;
+    }
+    //   console.log(`es par? ${par} y es mayus? ${mayus}`);
+    if (par === true && mayus === true) {
+      console.log("La palabra es par y empieza por mayuscula.");
+    } else if (par === true && mayus !== true) {
+      console.log("La palabra es par y no empieza por mayuscula.");
+    } else if (par !== true && mayus === true) {
+      console.log("La palabra no es par y empieza por mayuscula.");
+    } else {
+      console.log("La palabra no es par y no empieza por mayuscula.");
+    }
+    if (palabra.indexOf("ble") >= 0) {
+      console.log(`La palabra si contiene "ble"`);
+    } else {
+      console.log(`La palabra no contiene "ble"`);
+    }
+  });
+}
+analizaPalabras(array);
+analizaPalabras(array2);
